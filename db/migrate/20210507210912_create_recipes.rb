ae56b45a200b53597acd17c46313afb0c1194601
mrandolph83 <mrandolph83@gmail.com>
2021-05-07 17:24:02 -0400

    built migration tables

---
+++ db/migrate/20210507210912_create_recipes.rb
@@ -0,0 +1,15 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :content
+      t.string :category
+      t.string :source
+      t.integer :user_id
+      t.integer :simple_review
+      t.integer :healthy_review
+      t.integer :tasty_review
+      t.timestamps null: false
+    end
+  end
+end
